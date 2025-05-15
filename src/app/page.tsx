6d445dc7d773340c6b1da3680601f1687b12257e
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-05-15 15:07:37 +0300

    feat: create basic ProductCard component and update Home page to display products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
-import { Box, Typography } from "@mui/material";
+"use client";
+
+import { Box } from "@mui/material";
+import { Product } from "@/lib/types";
+import { useState } from "react";
+import ProductCard from "@/components/ProductCard";
+
+const DUMMY_PRODUCTS: Product[] = [
+  {
+    id: 1,
+    title: "Product 1",
+    price: 100,
+    description: "Description 1",
+    category: "Category 1",
+    image: "https://via.placeholder.com/150",
+    rating: {
+      rate: 4.5,
+      count: 10,
+    },
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    price: 200,
+    description: "Description 2",
+    category: "Category 2",
+    image: "https://via.placeholder.com/150",
+    rating: {
+      rate: 4.0,
+      count: 20,
+    },
+  },
+];
 
 export default function Home() {
-  // const { data, error, isLoading } = useQuery("products", getProducts);
+  const [products] = useState<Product[]>(DUMMY_PRODUCTS);
 
   return (
     <Box>
-      <Typography>asdsaad</Typography>
+      {products.map((item) => (
+        <ProductCard key={item.id} {...item} />
+      ))}
     </Box>
   );
 }
