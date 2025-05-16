63e7272fa82929fb85362b2c0b64df960c6fc162
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-05-16 09:32:20 +0300

    feat: enhance Searchbar component with product selection handling

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -4,9 +4,25 @@ import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
 import { useProductStore } from "../app/store/productStore";
+import { Product } from "@/lib/types";
+import { useState } from "react";
 
 export default function Searchbar() {
   const products = useProductStore((state) => state.products);
+  const [, setSelectedProduct] = useState<Product | null>(null);
+
+  const handleChange = (
+    event: React.SyntheticEvent,
+    value: string | Product | null
+  ) => {
+    if (typeof value === "object" && value !== null) {
+      setSelectedProduct(value);
+      console.log("Seçilen ürün objesi:", value);
+    } else {
+      setSelectedProduct(null);
+      console.log("Seçilen değer bir string veya null:", value);
+    }
+  };
 
   return (
     <Stack spacing={2} sx={{ width: 300, bgcolor: "white" }}>
@@ -14,7 +30,11 @@ export default function Searchbar() {
         id="Searchbar"
         freeSolo
         disableClearable
-        options={products.map((product) => product.title)}
+        options={products}
+        getOptionLabel={(option) =>
+          typeof option === "string" ? option : option.title
+        }
+        onChange={handleChange}
         renderInput={(params) => (
           <TextField
             {...params}
